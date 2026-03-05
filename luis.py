2c7db655177544f1a9e3831b4aec1dc6c089f9cc
ItsAntonio777 <luisvideostj@gmail.com>
2026-03-05 11:32:39 -0800

    Add Git flow commands and Python variable example
    Added basic Git flow commands and a Python variable example.

--- luis.py
+++ luis.py
@@ -1,5 +1,17 @@
+# Git Flow Basico
+Comando para el flujo de git basico
+```sh
+git status
+git add . o git add -a git status
+git commit -m "Descripcion del cambio en el proyecto" git push
+```
+## Nota
+Si no me dice quien soy,entonces necesito restaurar la PC. # Python
+Una variable en python es como sigue
+```
 nombre = "Anthony"
 numero = 777
 flotante = 3.1416
 booleano = True
 print(nombre, numero, flotante, booleano)
+```
